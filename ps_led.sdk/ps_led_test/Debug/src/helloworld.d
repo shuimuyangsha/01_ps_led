ec35533a17e35292dc700d159689892218cece57
Eden <1023178387@qq.com>
2019-07-17 20:52:00 +0800

    恢复工程

---
+++ ps_led.sdk/ps_led_test/Debug/src/helloworld.d
@@ -0,0 +1,46 @@
+src/helloworld.o src/helloworld.o: ../src/helloworld.c \
+ ../../ps_led_test_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../ps_led_test_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../ps_led_test_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../ps_led_test_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../ps_led_test_bsp/ps7_cortexa9_0/include/xgpiops.h \
+ ../../ps_led_test_bsp/ps7_cortexa9_0/include/xstatus.h \
+ ../../ps_led_test_bsp/ps7_cortexa9_0/include/xil_assert.h \
+ ../../ps_led_test_bsp/ps7_cortexa9_0/include/xgpiops_hw.h \
+ ../../ps_led_test_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../ps_led_test_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../ps_led_test_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../ps_led_test_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../ps_led_test_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h \
+ ../../ps_led_test_bsp/ps7_cortexa9_0/include/xplatform_info.h \
+ ../../ps_led_test_bsp/ps7_cortexa9_0/include/sleep.h
+
+../../ps_led_test_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../ps_led_test_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../ps_led_test_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../ps_led_test_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../ps_led_test_bsp/ps7_cortexa9_0/include/xgpiops.h:
+
+../../ps_led_test_bsp/ps7_cortexa9_0/include/xstatus.h:
+
+../../ps_led_test_bsp/ps7_cortexa9_0/include/xil_assert.h:
+
+../../ps_led_test_bsp/ps7_cortexa9_0/include/xgpiops_hw.h:
+
+../../ps_led_test_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../ps_led_test_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../ps_led_test_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../ps_led_test_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../ps_led_test_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
+
+../../ps_led_test_bsp/ps7_cortexa9_0/include/xplatform_info.h:
+
+../../ps_led_test_bsp/ps7_cortexa9_0/include/sleep.h:
